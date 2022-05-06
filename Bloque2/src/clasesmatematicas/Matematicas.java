c2f03b64cab1d2b90b4d6740c9e375a88edccc99
Victor Vera <victorveracoronel@live.com>
2022-05-06 18:35:44 +0200

    Añadir el proyecto al repositorio

---
+++ Bloque2/src/clasesmatematicas/Matematicas.java
@@ -0,0 +1,44 @@
+package clasesmatematicas;
+
+import modelo.Producto;
+
+/**
+ * Clase que contiene diferentes métodos matemáticos.
+ * @author Victor
+ */
+public class Matematicas {
+
+    /**
+     * Metodo que calcula el factorial de un numero.
+     * @param numero es el numero al que se le va a calcular el factorial
+     * @return  el factorial del numero pasado por parametro
+     */
+    public static int calcularFactorial(int numero) {
+        int factorial = 1;
+
+        for (int i = numero; i > 0; i--) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    /**
+     * Metodo que calcula el recargo que se le va a aplicar al precio de un producto
+     * @param producto es el producto al que se le va a aplicar el recargo
+     * @return el precio final del producto con el recargo aplicado y los gastos de envio
+     */
+    public float calcularRecargo(Producto producto) {
+        final float PORCENTAJE_RECARGO = 1.16f;
+        final short LIMITE = 300;
+        final short GASTOS_ENVIO = 10;
+        float recargo = 0;
+        if (producto.getPrecio() > LIMITE) {
+            recargo = producto.getPrecio() * PORCENTAJE_RECARGO;
+        }
+        recargo += GASTOS_ENVIO;
+
+        return recargo;
+    }
+
+}
